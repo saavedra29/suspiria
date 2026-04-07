21274cb6042bbab61f403ce2dda3a00b3afa6526
saavedra29 <arisgold29@gmail.com>
2026-04-07 18:08:44 +0300

    Reverse the order of the roles so that harvester has priority

--- src/main.ts
+++ src/main.ts
@@ -49,7 +49,7 @@ declare const global: {
     log: any;
 };
 
-const roles = [roleHarvester, upgrader, builder, repairer];
+const roles = [roleHarvester, upgrader, builder, repairer].reverse();
 
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
